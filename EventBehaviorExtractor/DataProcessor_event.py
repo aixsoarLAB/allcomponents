16c3acc4d6f1b589f712e26303394407b8dc4d70
WANGRoger123 <157264288+WANGRoger123@users.noreply.github.com>
2024-04-09 18:00:43 +0800

    事件轉序列程式合併”

--- EventBehaviorExtractor/DataProcessor_event.py
+++ EventBehaviorExtractor/DataProcessor_event.py
@@ -1,21 +1,31 @@
+import os
 import json
 import pandas as pd
 from pathlib import Path
 
 class DataProcessor:
-    def __init__(self, file_path, output_directories):
-        self.file_path = Path(file_path)
+    def __init__(self, file_directory, output_directories):
+        self.file_directory = Path(file_directory)
         self.output_directories = {k: Path(v) for k, v in output_directories.items()}
+        self.df = pd.DataFrame()
 
     def extract_data(self):
-        with open(self.file_path, 'r', encoding='utf-8') as file:
-            data = json.load(file)
-            records = [{
-                'src_ip': item.get('src_ip', ''),
-                'dest_ip': item.get('dest_ip', ''),
-                'signature_id': str(item['alert'].get('signature_id', ''))
-            } for item in data if 'alert' in item and 'signature_id' in item['alert']]
-        self.df = pd.DataFrame(records)
+        # 检查路径是否指向目录
+        if not self.file_directory.is_dir():
+            print(f"提供的路径{self.file_directory}不是一个目录")
+            return
+        
+        # 遍历目录下的所有.json文件
+        for file_path in self.file_directory.glob('*.json'):
+            with open(file_path, 'r', encoding='utf-8') as file:
+                data = json.load(file)
+                records = [{
+                    'src_ip': item.get('src_ip', ''),
+                    'dest_ip': item.get('dest_ip', ''),
+                    'signature_id': str(item['alert'].get('signature_id', ''))
+                } for item in data if 'alert' in item and 'signature_id' in item['alert']]
+                # 将当前文件的记录追加到DataFrame中
+                self.df = pd.concat([self.df, pd.DataFrame(records)], ignore_index=True)
 
     def write_to_files(self):
         # 將每個 IP 的 signature_id 列表保存到文件中
@@ -36,3 +46,29 @@ class DataProcessor:
     def run(self):
         self.extract_data()
         self.write_to_files()
+
+class EventIDExtractor:
+    def __init__(self, input_file_path, output_file):
+        self.input_file_path = input_file_path
+        self.output_file = output_file
+
+    def extract_event_ids(self):
+        file_names = sorted([f for f in os.listdir(self.input_file_path) if f.endswith('.json')])
+        with open(self.output_file, 'w', encoding='utf-8') as output_file:
+            for file_name in file_names:
+                file_path = os.path.join(self.input_file_path, file_name)
+                with open(file_path, 'r', encoding='utf-8') as file:
+                    data = json.load(file)
+                    event_ids = [str(item['_source']['winlog']['event_id']) 
+                                 for item in data['hits']['hits'] 
+                                 if 'event_id' in item['_source']['winlog']]
+                    output_file.write(' '.join(event_ids) + '\n')
+        print(f'所有 Event IDs 已成功儲存到 "{self.output_file}"。')
+
+    def clean_and_save(self):
+        with open(self.output_file, 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+        cleaned_lines = [line for line in lines if line.strip() != '']
+        with open(self.output_file, 'w', encoding='utf-8') as file:
+            file.writelines(cleaned_lines)
+        print(f'已整理檔案並儲存到 {self.output_file}')
--- EventBehaviorExtractor/main.py
+++ EventBehaviorExtractor/main.py
@@ -1,8 +1,9 @@
 from DataProcessor_event import DataProcessor
+from DataProcessor_event import EventIDExtractor
 
 
 # 請確保您的文件路徑是正確的，這裡使用原始字串表示法
-file_path =  r"D:\Users\admin\Desktop\data\Monday-WorkingHours\alerts_only.json"
+file_path =  'events/Monday'
 output_directories = {
     'Ssid': r"D:\project\allcomponents\EventBehaviorExtractor\sequence\Ssid\source_ip_signatures.txt",
     'Dsid': r"D:\project\allcomponents\EventBehaviorExtractor\sequence\Dsid\destination_ip_signatures.txt",
@@ -12,3 +13,9 @@ output_directories = {
 
 processor = DataProcessor(file_path, output_directories)
 processor.run()
+
+input_file_path = 'ids2018-54-new'
+output_file_path = 'sequence/ids2018-54'
+extractor = EventIDExtractor(input_file_path, output_file_path)
+extractor.extract_event_ids()
+extractor.clean_and_save()
